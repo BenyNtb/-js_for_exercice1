59669aaf48c5acd5a10dae54f881d5d123a8f146
Beny <b.ntambwe97@gmail.com>
2021-01-08 10:33:34 +0100

    fin exo + correction

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+// EXO 1
+// methode 1
+// for (let i = 0; i <= 20; i++) {
+//     console.log(i);;
+//     switch (i % 2 == 0) {
+//         case true:
+//             console.log(`${i} est un nombre pair`);
+//             break;
+//         case false:
+//             console.log(`${i} est un nombre impair`);
+//         default:
+//             break;
+//     }
+// }
+// methode 2 
+// if (i % 2 == 0) {
+//     console.log(`${i} est pair`);
+// } else {
+//     console.log(`${i} est impair`);
+// }
+// EXO 2
+// let coding16 = ['Beny', 'Zulma', 'Mohamed','Abdellah','Achraf', 'Adame', 'Alex', 'Ayhan', 'Ayoub', 'Louise', 'Malo', 'Maximilien', 'Maxence', 'Natchez', 'Saliou', 'Zakaria', 'Jamila', 'David']
+// for (let i = 0; i < coding16.length; i++) {
+//     console.log(`Bonjour ${coding16[i]}`);    
+// }
+// EXO 3
+// let nbr = parseInt(prompt("Entrez un nombre"))
+// if(!isNaN(nbr)){
+//     for(i=0; i<nbr; i++){
+//         console.log(i);
+//     }
+// } else {
+//     console.log(`Ce n'est pas un nombre`);
+// }
